1bc1a23326d71179573f0fdc5976ee3ee75ad350
bbaecco <46706915+bbaecco@users.noreply.github.com>
2024-12-31 18:30:58 +0900

    Title: 길이 단위 변환하기, Time: 95ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241231/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        float a = 9.2f, b = 1.3f;
+        System.out.printf("%.1fft = %.1fcm\n%.1fmi = %.1fcm", a, (a*30.48), b, (b*160934));
+    }
+}
